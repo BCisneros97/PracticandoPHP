0c2621a4371aac93bfbc40009a6b272c2ee5cb28
Benny Cisneros <benycisneros@gmail.com>
2017-10-07 10:21:27 -0500

    Lesson 5

--- index.php
+++ index.php
@@ -1,21 +1,5 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Welcome</title>
-        <style>
-            header {
-                background-color: gray;
-                padding: 2em;
-                text-align: center;
-            }
-        </style>
-    </head>
-    <body>
-        <header>
-            <h1>
-                <?= "Hello, " . htmlspecialchars($_GET['name']) ?>
-            </h1>
-        </header>
-    </body>
-</html>
+<?php
+
+$greeting = "Hello my friend";
+
+require "index.view.php";
---
+++ index.view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Welcome</title>
+        <style>
+            header {
+                background-color: gray;
+                padding: 2em;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+            <h1><?= $greeting ?></h1>
+        </header>
+    </body>
+</html>
